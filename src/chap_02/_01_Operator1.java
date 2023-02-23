e556897c7e6092c46266576873cbfe8c71e5a9e0
SoyoonPark <spurt@tukorea.ac.kr>
2023-02-24 00:40:19 +0900

    2023-02-24

--- src/chap_02/_01_Operator1.java
+++ src/chap_02/_01_Operator1.java
@@ -38,5 +38,34 @@ public class _01_Operator1 {
 
         c = a % b;
         System.out.println(c); // 0
+
+        // 증감 연산 ++, --
+        int val;
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(++val); // 11 -> ++ 연산자가 변수 앞에 붙었을 경우에는 ++val(1이 더해짐)이 먼저 수행되고 나서 sout 문장이 수행된다.
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val++); // 10 -> ++ 연산자가 변수 뒤에 붙었을 경우에는 sout 문장이 먼저 수행되고 나서 val++(1이 더해짐)이 수행된다.
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(--val); // 9
+        System.out.println(val); // 9
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val--); // 10
+        System.out.println(val); // 9
+
+        // 은행 대기번호 표
+        int waiting = 0;
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 0
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 1
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 2
+        System.out.println("총 대기 인원 : " + waiting); // 총 대기 인원 : 3
     }
 }
