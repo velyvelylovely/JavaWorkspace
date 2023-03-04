fac514ce8ae67f3ae8415cd990d9cc23a1edbe9a
SoyoonPark <spurt@tukorea.ac.kr>
2023-03-04 23:01:02 +0900

    2023-03-04

---
+++ src/chap_04/_08_NestedLoop.java
@@ -0,0 +1,45 @@
+package chap_04;
+
+import javax.swing.*;
+
+public class _08_NestedLoop {
+    public static void main(String[] args) {
+        // 이중 반복문
+
+        // 별 (*) 사각형 만들기
+        /*
+
+        *****
+        *****
+        *****
+        *****
+        *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("----------");
+
+        // 별 (*) 왼쪽 삼각형 만들기
+        /*
+
+        *
+        **
+        ***
+        ****
+        *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
