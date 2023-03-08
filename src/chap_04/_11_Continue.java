456cc0888e67005b1d8aa001eb1de21e23a16041
SoyoonPark <spurt@tukorea.ac.kr>
2023-03-08 23:58:05 +0900

    2023-03-08

--- src/chap_04/_11_Continue.java
+++ src/chap_04/_11_Continue.java
@@ -25,5 +25,29 @@ public class _11_Continue {
             }
         }
         System.out.println("영업을 종료합니다.");
+
+        System.out.println("------------------------------");
+
+        //While 문
+        sold = 0;
+        int index = 0; // 손님 번호
+        // while (index < 50) {
+        while (true) {
+            index++;
+            System.out.println(index + "번 손님, 주문하신 치킨 나왔습니다.");
+
+            // 손님이 없다연 ( noShow )
+            if (index == noShow) {
+                System.out.println(index + "번 손님, 노쇼로 인해 다음 손님에게 기회가 넘어갑니다.");
+                continue;
+            }
+
+            sold++; // 판매 처리
+            if (sold == max) {
+                System.out.println("금일 재료가 모두 소진되었습니다.");
+                break;
+            }
+        }
+        System.out.println("영업을 종료합니다.");
     }
 }
