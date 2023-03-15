c2daac07c5b2a1eef3694c1a491f32ffb7a0200e
SoyoonPark <spurt@tukorea.ac.kr>
2023-03-15 23:36:15 +0900

    2023-03-15

--- src/chap_05/_04_MultiArrayLoop.java
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -32,5 +32,27 @@ public class _04_MultiArrayLoop {
             }
             System.out.println();
         }
+
+        System.out.println("--------------------------");
+        // 세로 크기 10 x 가로 크기 15 에 해당하는 영화관 좌석
+        String[][] seats3 = new String[10][15];
+        String[] eng = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        for (int i = 0; i < seats3.length; i++) { // 세로
+            for (int j = 0; j < seats3[i].length; j++) {
+                seats3[i][j] = eng[i] + (j + 1);
+            }
+        }
+
+        // 표 구매
+        seats3[7][8] = "__"; // H9
+        seats3[7][9] = "___"; // H10
+
+        // 영화관 좌석 번호 확인
+        for (int i = 0; i < seats3.length; i++) {
+            for (int j = 0; j < seats3[i].length; j++) {
+                System.out.print(seats3[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
