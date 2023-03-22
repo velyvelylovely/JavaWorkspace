f3186714ae001b2af509f3646b5f7d8bc910eeb0
SoyoonPark <spurt@tukorea.ac.kr>
2023-03-22 16:52:58 +0900

    2023-03-22

--- src/chap_05/_04_MultiArrayLoop.java
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -34,7 +34,7 @@ public class _04_MultiArrayLoop {
         }
 
         System.out.println("--------------------------");
-        // 세로 크기 10 x 가로 크기 15 에 해당하는 영화관 좌석
+        // 세로 크기 10 x 가로 크기 15 에 해당하는 영화관 좌석 다차원 배열로 만들기
         String[][] seats3 = new String[10][15];
         String[] eng = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
         for (int i = 0; i < seats3.length; i++) { // 세로
@@ -47,7 +47,7 @@ public class _04_MultiArrayLoop {
         seats3[7][8] = "__"; // H9
         seats3[7][9] = "___"; // H10
 
-        // 영화관 좌석 번호 확인
+        // for 반복문을 통해 영화관 좌석 번호 확인
         for (int i = 0; i < seats3.length; i++) {
             for (int j = 0; j < seats3[i].length; j++) {
                 System.out.print(seats3[i][j] + " ");
