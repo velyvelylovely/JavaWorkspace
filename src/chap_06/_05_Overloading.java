3b7d4d53c6cbe0f7ccd3afa8a1fd4afe3fd59e76
SoyoonPark <spurt@tukorea.ac.kr>
2023-03-28 23:19:28 +0900

    2023-03-28

--- src/chap_06/_05_Overloading.java
+++ src/chap_06/_05_Overloading.java
@@ -1,22 +1,7 @@
 package chap_06;
 
 public class _05_Overloading {
-//    public static int getPower(int number) {
-//        int result = number * number;
-//        return result;
-//        // return number * number; -> 위 두줄은 이렇게 한 줄로 생략할 수 있다.
-//    }
-//
-//
-//    public static int getPowerByExp(int number, int exponent) {
-//        int result = 1;
-//        for (int i = 0; i < exponent; i++) {
-//            result *= number;
-//        }
-//        return result;
-//    }
     public static void main(String[] args) {
         // 메소드 오버로딩
-
     }
 }
